4d65ca773a128610a8cc870f191320231f7eb137
popigg <popigg@gmail.com>
2014-07-07 09:04:17 +0200

    modify routes criteria

--- routes/storiesHandler.js
+++ routes/storiesHandler.js
@@ -1,55 +1,9 @@
-module.exports = exports = function(app) {
+'use strict';
 
-	app.get('/', function(req, res) {
-		return res.render('index', { "name" : 'world' });
-	});
+var storiesController = require('../controller/storiesController.js');
 
-	app.get('/prueba1', function (req, res) {		
-		var firstN = Math.floor(Math.random() * 1000) + 1;
-		var secondN = Math.floor(Math.random() * 1000) + 1;
-		var resultSum = firstN + secondN;	
+module.exports = function(router) {
 
-		return res.render('prueba1', { 'firstN' : firstN,  'secondN': secondN, 'resultSum': resultSum });
-	});
-
-	app.post('/prueba1', function (req, res) {
-		var inputSum = req.body.inputSum;
-		var resultSum = req.body.resultSum;
-		var firstN = req.body.firstN;		
-		var secondN = req.body.secondN;		
-	
-
-		return res.render('prueba1', 
-					{ 
-						'firstN' : firstN,  
-						'secondN': secondN, 
-						'resultSum': resultSum, 
-						'inputSum': inputSum 
-					}
-				);
-	});
-
-	app.get('/prueba2', function (req, res) {
-				
-		return res.render('prueba2', {});
-	});
-
-	app.post('/prueba2', function (req, res) {
-		var inputResult = req.body.result;
-		var text = "En un lugar de La Mancha de cuyo nombre no quiero acordarme, vivía un hidalgo de los de ...";
-
-		var result = '';
-		for (var i = text.length - 1; i >= 0; i--) {
-			result += text[i];			
-		};
-
-		if (result == inputResult) {
-			var success = true;
-		} else {
-			var success = false;
-		}
-
-		return res.render('prueba2', {success: success});
-	});
-
+	router.route('/story/:id')
+        .get(storiesController.getStory);
 }
