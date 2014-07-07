2f243772fe2d0933da41a187d68c42b73ae42c2d
popigg <popigg@gmail.com>
2014-07-07 09:02:55 +0200

    scratch controllers idea

--- controllers/storiesController.js
+++ controllers/storiesController.js
@@ -1,3 +1,30 @@
-/**
- * Created by popigg on 05/07/2014.
- */
+'use strict';
+
+var Story = require('../models/story.js');
+
+exports.createStory = function(req, res) {
+    Story.create(req.body, function(error, story) {
+        if (error) {
+            res.send(501, error);
+            return;
+        }
+
+        res.send(story);
+    });
+};
+
+exports.getStories = function(req, res) {
+
+};
+
+exports.getStory = function(req, res) {
+
+};
+
+exports.updateStory = function(req, res) {
+
+};
+
+exports.removeStory = function(req, res) {
+
+};
