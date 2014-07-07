af9630aedfeec0152c6e7b15cf1065c50f4d345e
popigg <popigg@gmail.com>
2014-07-07 09:01:10 +0200

    work with mongoose new routes file

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-var express = require('express')
-	, app = express()
-	, cons = require('consolidate')
-	, routes = require('./routes');
+'use strict';
 
+/**
+ * required modules
+ * @type {*}
+ */
+var express     = require('express');
+var app         = express();
+var router      = express.Router();
+var cons        = require('consolidate');
+var routes      = require('./routes/storiesHandler.js');
 
-	app.engine('html', cons.swig);
-    app.set('view engine', 'html');
-    app.set('views', __dirname + '/views');    
+/**
+ * express configuration
+ */
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
-     // Application routes
-    routes(app);
+router.use('./routes/storiesHandler.js', router);
 
-    app.listen(3000);
-    console.log('Express server listening on port 3000');
+ // Application routes
+routes(app);
+
+app.listen(3000);
+console.log('Express server listening on port 3000');
