3531928dc3fea7effedcd10e0744eab618b62568
popigg <popigg@gmail.com>
2014-07-08 08:45:20 +0200

    routes implementation

--- routes/storiesHandler.js
+++ routes/storiesHandler.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var storiesController = require('../controller/storiesController.js');
+var userController = require('../controller/userController.js');
 
 module.exports = function(router) {
 
 	router.route('/story/:id')
-        .get(storiesController.getStory);
+        .post(storiesController.createStory)
+        .get(storiesController.getStory)
+        .put(storiesController.updateStory)
+        .remove(storiesController.removeStory);
 }
