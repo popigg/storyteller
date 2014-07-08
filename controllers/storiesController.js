62411944571c0b57c1925e6f565f170f26826aef
popigg <popigg@gmail.com>
2014-07-08 08:45:01 +0200

    controller implementation

--- controllers/storiesController.js
+++ controllers/storiesController.js
@@ -13,18 +13,46 @@ exports.createStory = function(req, res) {
     });
 };
 
-exports.getStories = function(req, res) {
-
-};
-
 exports.getStory = function(req, res) {
+    Story.findById(req.param.Id, function(error, story){
+        if (error) {
+            res.send(501, error);
+            return;
+        }
 
+        res.send(story)
+    });
 };
 
 exports.updateStory = function(req, res) {
+    Story.findOneAndUpdate(req.params.Id, req.body, function(errror, story){
+        if (error) {
+            res.send(501, error);
+            return;
+        }
 
+        res.send(story);
+    });
 };
 
 exports.removeStory = function(req, res) {
+    Story.findOneAndRemove(req.params.Id, function(errror, story){
+        if (error) {
+            res.send(501, error);
+            return;
+        }
 
+        res.send(story);
+    });
+};
+
+exports.getStories = function(req, res) {
+    Story.find(function(error, stories) {
+       if (error) {
+           res.send(501, error);
+           return;
+       }
+
+       res.send(stories);
+    });
 };
